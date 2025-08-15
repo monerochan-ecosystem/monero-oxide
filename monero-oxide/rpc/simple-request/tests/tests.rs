94b9777150cca0f7a99f41d9aa1b00072801ea63
Luke Parker <lukeparker5132@gmail.com>
2025-08-15 12:53:28 -0400

    August 2025 versions of the Rust compiler
    Applies changes from fmt/clippy.

--- monero-oxide/rpc/simple-request/tests/tests.rs
+++ monero-oxide/rpc/simple-request/tests/tests.rs
@@ -19,8 +19,7 @@ async fn test_rpc() {
 
   let guard = SEQUENTIAL.lock().await;
 
-  let rpc =
-    SimpleRequestRpc::new("http://monero:oxide@127.0.0.1:18081".to_string()).await.unwrap();
+  let rpc = SimpleRequestRpc::new("http://monero:oxide@127.0.0.1:18081".to_string()).await.unwrap();
 
   {
     // Test get_height
@@ -70,8 +69,7 @@ async fn test_decoy_rpc() {
 
   let guard = SEQUENTIAL.lock().await;
 
-  let rpc =
-    SimpleRequestRpc::new("http://monero:oxide@127.0.0.1:18081".to_string()).await.unwrap();
+  let rpc = SimpleRequestRpc::new("http://monero:oxide@127.0.0.1:18081".to_string()).await.unwrap();
 
   // Ensure there's blocks on-chain
   rpc
